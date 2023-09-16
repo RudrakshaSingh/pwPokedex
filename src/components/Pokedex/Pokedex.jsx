bdaf3e8e92e51652f5bb104106aecdc5e7fcec73
RudrakshaSingh <rudrakshasingh587@gmail.com>
2023-09-16 08:15:54 -0700

    pokedex.jsx

--- src/components/Pokedex/Pokedex.jsx
+++ src/components/Pokedex/Pokedex.jsx
@@ -4,11 +4,7 @@ import "./Pokedex.css";
 
 function Pokedex() {
     return (
-        <div className="pokedex-wrapper">
-            <h1 className="pokedex-heading">Pokedex</h1>
-            <Search />
-            <PokemonList />
-        </div>
+        
     );
 }
 export default Pokedex;
