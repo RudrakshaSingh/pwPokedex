aada6d90a33dd9b9da37e02d462bf9d80a5d2763
RudrakshaSingh <rudrakshasingh587@gmail.com>
2023-09-21 02:59:07 -0700

    cleaned up the states

--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -1,31 +1,53 @@
 import { useEffect, useState } from "react";
+//queueinga series of set updates=note set state update cannot be used multiple times ata time it will take last one only in that case
+//It is an uncommon use case, but if you would like to update the same state variable multiple times before the next render, instead of passing the next state value like setNumber(number + 1), you can pass a function that calculates the next state based on the previous one in the queue, like setNumber(n => n + 1).
 import axios from "axios";
 import "./PokemonList.css";
 import Pokemon from "../Pokemon/Pokemon";
 function PokemonList() {
-    const [pokemonList, setPokemonList] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
+    // const [pokemonList, setPokemonList] = useState([]);
+    // const [isLoading, setIsLoading] = useState(true);
 
-    const [pokedexUrl, setPokedexUrl] = useState("https://pokeapi.co/api/v2/pokemon");
-    const [nextUrl, setNextUrl] = useState("");
-    const [prevUrl, setPrevUrl] = useState("");
+    // const [pokedexUrl, setPokedexUrl] = useState("https://pokeapi.co/api/v2/pokemon");
+
+    // const [nextUrl, setNextUrl] = useState("");
+
+    // const [prevUrl, setPrevUrl] = useState("");
+
+    const [pokemonListState, setPokemonListState] = useState({
+        pokemonList: [],
+        isLoading: true,
+        pokedexUrl: "https://pokeapi.co/api/v2/pokemon",
+        nextUrl: "",
+        prevUrl: "",
+    });
 
     async function downloadPokemons() {
-        setIsLoading(true);
+        // setIsLoading(true);
+        setPokemonListState((state) => ({ ...state, isLoading: true }));
+        const response = await axios.get(pokemonListState.pokedexUrl); //this downloads list of 20 pokemons
 
-        const response = await axios.get(pokedexUrl); //this downloads list of 20 pokemons
         const pokemonResults = response.data.results; //we get the array of pokemons
-        console.log(pokemonResults);
 
-        //iterating over the array of pokemons and using their url to create an array of promises that will download those 20 pokemons
+        console.log("respnose is", response.data, "jjjjj", response.data.next);
+        console.log("kj", pokemonListState);
+        setPokemonListState((state) => ({
+            ...state,
+            nextUrl: response.data.next,
+            prevUrl: response.data.previous,
+        }));
+
+        //iterating over the array of pokemons and using their url to create an array of promises
+        // that will download those 20 pokemons
         const pokemonResultPromise = pokemonResults.map((pokemon) => axios.get(pokemon.url));
 
         //passing that promise array to axios.all to get array of 20 pokemons with detailed data
         const pokemonData = await axios.all(pokemonResultPromise); //axios.all allow us to pass an array of promises when all data is downloaded
+
         console.log(pokemonData);
 
         //now iterate on the data of each pokemon and exrtrating id,name image,types
-        const res = pokemonData.map((pokeData) => {
+        const pokeListResult = pokemonData.map((pokeData) => {
             const pokemon = pokeData.data;
             return {
                 id: pokemon.id,
@@ -34,26 +56,46 @@ function PokemonList() {
                 types: pokemon.types,
             };
         });
-        console.log(res);
-        setNextUrl(response.data.next);
-        setPrevUrl(response.data.previous);
-
-        setPokemonList(res);
-        setIsLoading(false);
+        console.log("hi", pokeListResult);
+        setPokemonListState((state) => ({
+            ...state,
+            pokemonList: pokeListResult,
+            isLoading: false,
+        }));
     }
     useEffect(() => {
         downloadPokemons();
-    }, [pokedexUrl]); //callback the content will execute when element first get rendered.if dependancy array is not given if will execute callback everytime component renders.if pass dependency array if will execute only first time
+    }, [pokemonListState.pokedexUrl]); //callback the content will execute when element first get rendered.if dependancy array is not given if will execute callback everytime component renders.if pass dependency array if will execute only first time
     //we can give [x] to execute after changes in x only.to track particular variables
 
     return (
         <div className="pokemon-list-wrapper">
-            <div className="pokemon-wrapper">{isLoading ? "Loading..." : pokemonList.map((p) => <Pokemon name={p.name} image={p.image} key={p.id} id={p.id} />)}</div>
+            <div className="pokemon-wrapper">{pokemonListState.isLoading ? "Loading..." : pokemonListState.pokemonList.map((p) => <Pokemon name={p.name} image={p.image} key={p.id} id={p.id} />)}</div>
             <div className="controls">
-                <button disabled={prevUrl == null} onClick={() => setPokedexUrl(prevUrl)}>
+                <button
+                    disabled={pokemonListState.prevUrl == null}
+                    onClick={() => {
+                        const urlToSet = pokemonListState.prevUrl;
+                        setPokemonListState((state) => ({
+                            ...state,
+                            pokedexUrl: urlToSet,
+                        }));
+                    }}
+                >
                     Prev
                 </button>
-                <button disabled={nextUrl == null} onClick={() => setPokedexUrl(nextUrl)}>
+                <button
+                    disabled={pokemonListState.nextUrl == null}
+                    onClick={() => {
+                        console.log(pokemonListState);
+                        const urlToSet = pokemonListState.nextUrl;
+
+                        setPokemonListState((state) => ({
+                            ...state,
+                            pokedexUrl: urlToSet,
+                        }));
+                    }}
+                >
                     Next
                 </button>
             </div>
